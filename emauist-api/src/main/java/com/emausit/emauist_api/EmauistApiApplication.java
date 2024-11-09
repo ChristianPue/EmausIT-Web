deb52d277888184e2f55d73a9d36d5db313eeac7
ChristianPue <christianaraujoc11@gmail.com>
2024-11-09 09:54:54 -0500

    commit 1 java spring boot base

---
+++ emauist-api/src/main/java/com/emausit/emauist_api/EmauistApiApplication.java
@@ -0,0 +1,13 @@
+package com.emausit.emauist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmauistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmauistApiApplication.class, args);
+	}
+
+}
---
+++ emauist-api/src/test/java/com/emausit/emauist_api/EmauistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emausit.emauist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmauistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
